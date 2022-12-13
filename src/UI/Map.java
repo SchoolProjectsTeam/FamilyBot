06e0a72cbefa4118e54de73b7332caa6a6a0f50b
Dr <Dr@DESKTOP-N4CR315>
2022-12-12 19:46:15 -0500

    Pue... ya el mapa se genera bro, solo queda hacer lo obstaculos, inicio y meta!

--- src/UI/Map.java
+++ src/UI/Map.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.DimensionUIResource;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -22,13 +23,24 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
 import java.awt.Panel;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+import java.awt.CardLayout;
 
 public class Map extends JFrame {
 
 	private JPanel contentPane;
+	JPanel mapPanel = new JPanel();
 	private ArrayList<JButton> casillas;
-	private JButton btnNewButton = new JButton("New button");
 	private int coordX;
 	private int coordY;
 	/**
@@ -39,16 +51,10 @@ public class Map extends JFrame {
 		coordY = y;
 		casillas = new ArrayList<JButton>();
 		initComponents();
-		
+		dibujarMapa(coordX, coordY);
 
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				dibujarMapa(coordX, coordY);
-			}
-		});
-		
-		
-		
+
+
 	}
 	public void initComponents(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -56,20 +62,27 @@ public class Map extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[][][][][][][][][][][][][][][][][][][][][][]", "[23px][][][][][][][][][][][][][][][][]"));
-		contentPane.add(btnNewButton, "cell 21 16,alignx left,aligny top");
+		contentPane.setLayout(new MigLayout("", "[711px]", "[493px]"));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, "cell 0 0,grow");
+		
+		
+		scrollPane.setViewportView(mapPanel);
+		mapPanel.setLayout(new MigLayout("", "[]", "[]"));
+
 	}
-	
+
 	public void dibujarMapa(int x, int y){
 		for(int i = 0; i<x; i++){
 			for(int j = 0; j<y; j++){
-			JPanel panel = new JPanel();
-			panel.setBackground(Color.RED);
-			contentPane.add(panel, "cell" + i + " " + j );
+				JPanel panel = new JPanel();
+				panel.setBackground(Color.RED);
+				DimensionUIResource a = new DimensionUIResource(100, 100);
+				panel.setPreferredSize(a);
+				mapPanel.add(panel, "cell" + i + " " + j );
 			}
+			mapPanel.updateUI();	
 		}
-		contentPane.updateUI();	
 	}
-	
-	
 }
