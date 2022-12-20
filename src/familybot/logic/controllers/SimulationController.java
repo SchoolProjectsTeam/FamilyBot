f9a831813b1be386b0f5c2e1efc253d36fdd68f3
Carlos Robaina <cdrobayna01@gmail.com>
2022-12-20 17:09:37 -0500

    Documentacion anadida

--- src/familybot/logic/controllers/SimulationController.java
+++ src/familybot/logic/controllers/SimulationController.java
@@ -27,21 +27,41 @@ public class SimulationController {
     	files = new FileControllerDAT();
     }
     
+    /**
+     * Devuelve la familia actualmente seleccionada
+     * @return Family
+     */
     public Family getFamily() {return family;}
     
+    /**
+     * Selecciona la familia a utilizar, recibe el indice de dicha familia en el arreglo de familias
+     * @param index
+     */
     public void selectFamily(int index) {
     	family = families.get(index);
     }
     
+    /**
+     * Crea una familia nueva, recibe el mapa ya creado, se crea en la interfaz
+     * @param map
+     */
     public void createFamily(Board map) {
-    	family = new Family(map);
+    	this.family = new Family(map);
+    	families.add(this.family);
     }
     
+    /**
+     * Devuelve un clon del arreglo de familias guardado en el archivo, los cambios hechos a este arreglo no se aplican a los originales, su objetivo es solo obtener el indice de dichas familias
+     * @return ArrayList de Familias
+     */
     @SuppressWarnings("unchecked")
 	public ArrayList<Family> loadFamilies() {
     	return (ArrayList<Family>) files.read().clone();
     }
     
+    /**
+     * Guarda en el fichero el arreglo de familias
+     */
     public void saveFamilies() {
     	files.write(families);
     }
