7a82b51c5599e7444b5b3e163fb644eeab437840
Carlos Robaina <cdrobayna01@gmail.com>
2022-12-20 15:05:12 -0500

    b

--- src/UI/UiUtil.java
+++ src/UI/UiUtil.java
@@ -6,7 +6,7 @@ import familybot.logic.core.Coordinate;
 public class UiUtil {
 	public Coordinate generateRandomCoordinate(int a, int b, Board board){
 		int x = (int)(Math.random()*a);
-		int y = (int)(Math.random()*b);
-		return new Coordinate(x, y, board);		
+		int y = (int)(Math.random()*b);	
+		return board.makePosition(x, y);
 	}
 }
